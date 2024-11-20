70883b62ac27dc18ce5a8b2996f50e6d9d71b765
Demis Bellot <demis.bellot@gmail.com>
2024-11-20 16:00:10 +0800

    hard code baseUrl

--- MyApp.Client/Program.cs
+++ MyApp.Client/Program.cs
@@ -9,11 +9,13 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
+var baseUrl = "https://blazor-wasm.web-templates.io";
+
 // Use / for local or CDN resources
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
-builder.Services.AddBlazorApiClient(apiBaseUrl);
+builder.Services.AddBlazorApiClient(baseUrl);
 builder.Services.AddLocalStorage();
 
 await builder.Build().RunAsync();
